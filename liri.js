85beb18ab05e4ea6433c4ff670a4649fee4fa34f
BlueCam74 <mabdulha@uci.edu>
2019-01-20 22:11:33 -0800

    working draft

--- liri.js
+++ liri.js
@@ -1,84 +1,107 @@
 require("dotenv").config();
 var axios = require("axios");
 var fs = require("fs");
+var keys = require("./keys.js");
+var Spotify = require('node-spotify-api');
+var request = require("request");
+
+
 
 var search = process.argv[2];
 var term = process.argv.slice(3).join(" ");
 
-
-if (!search) {
-  search = "show";
-}
-
-if (!term) {
-  term = "Andy Griffith";
-}
-
-if (search === "spotify-this-song") {
+function main(){
+if (search === "spotify-this-song" && term) {
   console.log("Searching for Song");
-  liriBot.findSong(term);
+  findSong(term);
 }
-else if (search === "movie-this") {
+else if (search === "movie-this" && term) {
     console.log("Searching for Movie");
-  liriBot.findMovie(term);
+   findMovie(term);
+}
+else if (search === "spotify-this-song" && !term){
+    console.log("Spotifying 'The Sign'");
+    term = "The Sign ace of base";
+    findSong(term);
+}
+else if (search === "movie-this" && !term) {
+    console.log("Searching for 'Mr. Nobody'");
+    term = "Mr. Nobody";
+    findMovie(term);
+}
+else if (search === "do-what-it-says") {
+  console.log("Spotifying 'I Want It That Way'");
+  readRandom();
+  console.log(term);
 }
 else {
-  console.log("Spotifying 'I Want It That Way'");
-  term = "I want it that way";
-  liriBot.findSong(term);
+    console.log("Please enter a valid command.")
+}
+};
+function grabTerm(search2, term2){
+    search = search2;
+    term = term2;
+    console.log(term);
+    main();
+
 }
 
-
-var liriBot = function () {
-    var divider = "\n-------------------------------------------------\n";
-    this.findSong = function (song) {
-        // The following URL can be used to search the TV Maze API for a given show
-        var URL = "http://api.tvmaze.com/singlesearch/shows?q=" + song;
-        console.log(typeof (URL));
-        axios.get(URL)
-            .then(function (response) {
-                var showRespData = response.data;
-                var showData = [
-                    divider,
-                    "Show: " + showRespData.name,
-                    "Genre(s): " + showRespData.genres.join(),
-                    "Rating: " + showRespData.rating.average,
-                    "Network: " + showRespData.network.name,
-                    "Summary: " + showRespData.summary
-                ].join("\n");
-            console.log(showRespData);
-            fs.appendFile('log.txt', showData, function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-        });
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+function readRandom() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+          return console.log(error);
         }
-    this.findMovie = function (movie) {
-        // The following URL can be used to search the TV Maze API for a given show
-        var URL = "http://api.tvmaze.com/search/shows?q=" + movie;
-        axios.get(URL)
-            .then(function (response) {
-                var person = response.data[0];
-                console.log(person);
-                var actorData = [
-                    divider,
-                    "Name: " + person.name,
-                    "Gender: " + person.gender,
-                    "Birthday: " + person.birthday,
-                    "Country: " + person.country.name,
-                    "TV Maze URL: " + person.url
-                ].join("\n");
-            
-            fs.appendFile('log.txt', actorData, function (err) {
-            if (err) throw err;
-            console.log('Saved!');
-                });
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    };
+        var dataArr = data.split(",");
+        var search2 = dataArr[0];
+        var term2 = dataArr[1];
+        grabTerm(search2, term2);
+      
+      });
+      
+
+}
+
+ function findSong(term) {
+    var spotify = new Spotify(keys.spotify);
+    spotify.search({ type: 'track', query: term }, function (err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        } else {
+            var results = [
+                "-------------------------------------------------------------",
+                "Artist Name: " + data.tracks.items[0].album.artists[0].name,
+                "\nSong Name: " + data.tracks.items[0].name, 
+                "\nPreview URL: " + data.tracks.items[0].album.external_urls.spotify,
+                "\nAlbum Name: " + data.tracks.items[0].album.name
+            ].join("\n");
+            console.log(results);
+        }
+    });
     }
+function findMovie(term) {
+        request(`http://www.omdbapi.com/?t=${term}&y=&plot=short&apikey=trilogy`, function(error, response, body) {
+        
+          if (!error && response.statusCode === 200) {
+ 
+            var result = [
+                "-------------------------------------------------------------",
+                "Title: " + JSON.parse(body).Title,
+                "Year: " + JSON.parse(body).Year,
+                "IMBD Rating: " + JSON.parse(body).imdbRating,
+                "Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value,
+                "Country Produced: " + JSON.parse(body).Country,
+                "Language: " + JSON.parse(body).Language,
+                "Plot: " + JSON.parse(body).Plot,
+                "Actors: " + JSON.parse(body).Actors
+            ].join("\n");
+
+            console.log(result);
+
+          }
+        });               
+            
+    };
+    
+
+main();
